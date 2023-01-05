f7c37210b28d1db0efecb08669f300e82366230f
kostyantyn <kost@yan.me>
2023-01-06 01:58:20 +0200

    remove using pandas because problems with build

--- Controller/add_data_screen.py
+++ Controller/add_data_screen.py
@@ -5,11 +5,11 @@ import View.AddDataScreen.add_data_screen
 # We have to manually reload the view module in order to apply the
 # changes made to the code on a subsequent hot reload.
 # If you no longer need a hot reload, you can delete this instruction.
-importlib.reload(View.AddDataScreen.add_data_screen)
-import pandas as pd
+# importlib.reload(View.AddDataScreen.add_data_screen)
+#import pandas as pd
 import pathlib
-
-
+import csv
+from collections import defaultdict
 
 
 class AddDataScreenController:
@@ -20,14 +20,21 @@ class AddDataScreenController:
     the view to control its actions.
     """
     tree_values_path = pathlib.Path("assets", "tree_values.csv").resolve()
-    tree_suggestions_df = pd.read_csv(tree_values_path, index_col=False)[['value', 'latin_name']]
-
-
+    # tree_suggestions_df = pd.read_csv(tree_values_path, index_col=False)[['value', 'latin_name']]
+    columns = defaultdict(list)
+    tree_suggestions = None
 #    suggestions = ['olive', 'olive1', 'olive2', 'olive3', 'black', 'white', 'red', 'reggy', 'babydible']
 
     def __init__(self, model):
         self.model = model  # Model.add_data_screen.AddDataScreenModel
         self.view = View.AddDataScreen.add_data_screen.AddDataScreenView(controller=self, model=self.model)
+
+        with open(self.tree_values_path) as f:
+            reader = csv.DictReader(f)  # read rows into a dictionary format
+            for row in reader:  # read a row as {column1: value1, column2: value2,...}
+                for (k, v) in row.items():  # go over each column name and value
+                    self.columns[k].append(v)
+        self.tree_suggestions = self.columns['latin_name']
 
         self.new_record_dict = {}
 
@@ -42,10 +49,10 @@ class AddDataScreenController:
         self.new_record_dict[feature_key] = feature_value
 
     def find_suggestions(self, text: str)->list[str]:
-        out_df = self.tree_suggestions_df[self.tree_suggestions_df['latin_name'].str.contains(text)]
-        print("out df: ", out_df['latin_name'].tolist())
-        # return list(filter(lambda sugg: sugg.lower().startswith(text.lower()), self.tree_suggestions_df))
-        return out_df['latin_name'].tolist()
+        # out_df = self.tree_suggestions[self.tree_suggestions['latin_name'].]
+        # print("out df: ", out_df['latin_name'].tolist())
+        return list(filter(lambda sugg: sugg.lower().startswith(text.lower()), self.tree_suggestions))
+        # return out_df['latin_name'].tolist()
 
 
     def write_record_to_json(self):
